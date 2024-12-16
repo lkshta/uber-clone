eb467451f8cfde33dff5d436f829d0133a2c0554
Lakshita <2003lakshita@gmail.com>
2024-12-16 22:55:41 +0530

    register captain

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,7 +1,7 @@
 const userModel = require("../models/user.model");
 const userService = require("../services/user.service");
 const { validationResult } = require("express-validator");
-const blackListTokenModel = require('../models/blackListToken.model');
+const blackListTokenModel = require("../models/blackListToken.model");
 
 module.exports.registerUser = async (req, res, next) => {
   const errors = validationResult(req);
@@ -11,11 +11,11 @@ module.exports.registerUser = async (req, res, next) => {
 
   const { fullname, email, password } = req.body;
 
-  // const isUserAlready = await userModel.findOne({ email });
+  const isUserAlready = await userModel.findOne({ email });
 
-  // if (isUserAlready) {
-  //     return res.status(400).json({ message: 'User already exist' });
-  // }
+  if (isUserAlready) {
+    return res.status(400).json({ message: "User already exist" });
+  }
 
   const hashedPassword = await userModel.hashPassword(password);
 
